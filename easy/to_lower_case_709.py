da1cd6afd27f9cb44969d74c35e03eabcd8afd8e
yangxin_ryan <yangxin03@youxin.com>
2020-06-12 09:14:20 +0800

    更新

---
+++ easy/to_lower_case_709.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+__author__ = 'ryan_ryan'
+"""
+Solutions:
+字符串的大写转小写，直接内置函数，或者利用Ascii转换即可；
+"""
+
+
+class ToLowerCase(object):
+
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
