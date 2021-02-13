6f092ac1868d3fcdbaf1e4fc0de4ebfcc2aa18bc
yangxin_ryan <yangxin03@youxin.com>
2021-02-14 00:59:27 +0800

    add FairCandySwap

--- easy/fair_candy_swap.py
+++ easy/fair_candy_swap.py
@@ -1,6 +1,14 @@
 # -*- coding:utf-8 -*-
 __author__ = 'yangxin_ryan'
-
+"""
+Solutions:
+题意：
+给出两个数组A、B
+我们需要交换A、B中的数字，达到A、B两个数组分别求和相等；
+思路：
+我们可以分别算下 (SUM(A) + SUM(B)) / 2 是多少，
+这样我们去算下，A中去掉什么，并且加上B中的数字刚好凑到就是答案； 
+"""
 
 class FairCandySwap(object):
 
