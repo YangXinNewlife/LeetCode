eb92a0f3cb5af3ebbe80f3f3c4fc876a8c99d763
YangXinNewlife <yangxin812716131@163.com>
2021-09-21 11:20:52 +0800

    add commir

--- easy/cousins_in_binary_tree_993.py
+++ easy/cousins_in_binary_tree_993.py
@@ -1,6 +1,14 @@
 # -*- coding:utf-8 -*-
 __author__ = 'yangxin_ryan'
 import collections
+"""
+Solutions:
+题目的含义是找出来二叉树的兄弟节点。
+兄弟节点定义：
+高度相同，父节点不同。
+我们使用深度遍历，并记录下高度和父节点，最后比较即可。
+由于每个节点不同我们可以使用set来记录。
+"""
 
 
 class CousinsInBinaryTree(object):
