31cc44d3dc13e84662e0506943e0b7577fee6231
YangXinNewlife <yangxin812716131@163.com>
2023-04-08 01:02:36 +0800

    add DetermineIfStringHalvesAreAlike

--- easy/determine_if_string_halves_are_alike_1704.py
+++ easy/determine_if_string_halves_are_alike_1704.py
@@ -2,10 +2,29 @@
 __author__ = 'yangxin_ryan'
 """
 Solutions:
-
+题意是给定一个字符串s,
+将字符串s拆分称为两等分长度字符串fst_str和snd_str。
+然后分别统计fst_str和snd_str中分别包含aeiou和AEIOU的个数，
+最后判断下是否一致即可。
 """
+
 
 class DetermineIfStringHalvesAreAlike(object):
 
     def halvesAreAlike(self, s: str) -> bool:
+        fst_str = s[:len(s) // 2]
+        snd_str = s[len(s) // 2:]
+        vowel = "aeiouAEIOU"
+        fst_cnt = 0
+        snd_cnt = 0
+        for i in range(len(fst_str)):
+            if fst_str[i] in vowel:
+                fst_cnt += 1
+            if snd_str[i] in vowel:
+                snd_cnt += 1
+        return fst_cnt == snd_cnt
+
+
+
+
 
